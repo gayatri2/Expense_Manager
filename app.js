449c981abe615974a8c76cd32e7406109bbe1544
gayatri2 <gayatri.godbole02@gmail.com>
2021-03-08 09:04:17 +0530

    commit6
    Values input by the user are stored in expensesDB database.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const ejs = require("ejs");
+const _ = require("lodash");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
@@ -10,15 +11,36 @@ app.set('view engine', 'ejs');
 
 const mongoose=require('mongoose');
 const encrypt = require("mongoose-encryption");
+global.email="";
+global.b1="";
+global.flag=0;
 mongoose.connect('mongodb+srv://g3:g3@cluster0.9owf4.mongodb.net/userDB',{ useNewUrlParser: true, useUnifiedTopology: true } ); 
 
 const userSchema=new mongoose.Schema({  
   email:String,
-  password:String
+  password:String,
+  budget:Number
 });
+
+
+const expenseSchema=new mongoose.Schema({  
+  email:String,
+  category:String,
+  cost:Number,
+  item: String,
+  date:
+  {
+    type:Date, 
+    default:Date.now()
+  },
+  delete:Boolean
+  
+});
+
 const secret = "Thisisourlittlesecret.";
 userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 const User=new mongoose.model("User",userSchema);
+const Expense = new mongoose.model("Expense",expenseSchema);
 
 app.use(express.static("public"));
 app.get("/", function (req,res){
@@ -29,18 +51,51 @@ app.get("/register", function (req,res)
     res.sendFile(__dirname+"/register.html")
 });
 app.get("/home", function (req,res){
-  res.sendFile(__dirname+"/home.html")});
+  if(flag===1)
+  {
+    res.sendFile(__dirname+"/home.html");
+  }
+  else{
+    res.sendFile(__dirname+"/index.html");
+  }
+});
+  
 app.get("/dashboard", function (req,res){
-    res.sendFile(__dirname+"/dashboard.html")});
+    if(flag===1)
+    {
+      res.sendFile(__dirname+"/dashboard.html");
+    }
+    else
+    {
+      res.sendFile(__dirname+"/index.html");
+    }
+});
 app.get("/settings", function (req,res){
-      res.sendFile(__dirname+"/settings.html")});
+    if(flag===1)
+    {
+      res.sendFile(__dirname+"/settings.html");
+    }
+    else
+    {
+      res.sendFile(__dirname+"/index.html");  
+    }
+});
 app.get("/logout",function(req,res){
-
-  res.redirect("/");
+  if(flag===1)
+  {
+    flag=0;
+    res.redirect("/");
+  }
+  else
+  {
+    res.redirect("/");
+  }
+  
 });
 
  
 app.post("/register",function(req,res){
+  email=req.body.email;
   const newUser =  new User({
     email: req.body.email,
     password: req.body.password
@@ -49,6 +104,7 @@ app.post("/register",function(req,res){
     if (err) {
       console.log(err);
     } else {
+      flag=1;
       res.sendFile(__dirname+"/home.html")      
 
     }
@@ -56,14 +112,16 @@ app.post("/register",function(req,res){
 
 });
 app.post("/login",function(req,res){
-  const email = req.body.email;
+  email=req.body.email;
   const password = req.body.password;
   User.findOne({email: email}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
+
       if (foundUser) {
         if (foundUser.password === password) {
+          flag=1;
           res.sendFile(__dirname+"/home.html")
         }
       }
@@ -73,20 +131,56 @@ app.post("/login",function(req,res){
 });
 app.post("/budget",function(req,res){
   const budget=Number(req.body.budget);
+  b1=req.body.budget;
+
   console.log(budget);
+  User.updateOne({email:email},{budget:budget},function(err){
+   if(err){
+     console.log(err);
+   }
+   else{
+     console.log('Budget updated in the DB');
+   }
+ })
   
   res.redirect("/settings");
  
 });
 
 app.post("/expenditure",function(req,res){
+  const radio=req.body.optradio;
   const item=req.body.item;
   const cost=Number(req.body.cost);
-  console.log(item); console.log(cost);
-  const radio=req.body.optradio;
+  b1=b1-cost;
+  console.log(b1);
+  User.updateOne({email:email},{budget:b1},function(err){ //upadating budget in the database
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log('Budget updated in the DB');
+    }
+  }); 
   console.log(radio);
-  res.redirect("/settings");
-  
+  console.log(item);
+  console.log(cost);
+  const newExpense= new Expense({
+    email: email,
+    category: radio,
+    item: item,
+    cost: cost,
+    delete:0
+  });
+  newExpense.save(function(err){
+    if(err)
+    {
+      console.log(err);
+    }
+    else{
+      res.sendFile(__dirname+"/settings.html");
+    }
+  });
+
 });
 app.listen(3000, function() {
     console.log("Server started on port 3000");
