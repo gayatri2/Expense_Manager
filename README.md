631a1ca43879ff157a5ab14c5cc6371f61202b14
gayatri2 <gayatri.godbole02@gmail.com>
2021-05-02 09:25:03 +0530

    UI updated. Responsiveness feature added.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# fullstack_task
+This is a budget manager project.
 
-This is a simple expense manager project.
-
-Technologies used: HTML, CSS, Javascript, Mongo Db.
-Dependencies: Node js, Express js, mongoose.
+The technologies used in this project are: Html, Css, Javascript, Mongo Db.
+The dependencies are: Node js, Express js, mongoose.
 
 In this project, initially, there is an authentication system.
 This system, has two parts, login and register.
@@ -14,7 +12,7 @@ a) Dashboard
 b)Settings
 c)Log Out
 
-On the settings page, user can add budget, select its category(using radio buttons), enter the item and cost.
+On the settings page, user can add budget, select its category, enter the item and cost.
 
 On the Dashboard page, first, the budget of the user is displayed.Then, there are three buttons.
 Add expense, view expense and Update expense.
@@ -26,20 +24,29 @@ The item does not get deleted from the database.When an item is deleted, the bud
 c)Update expense lets the user to update item name as well as its cost. When cost is updated, the budget amount also changes.
 
 The log out button logs the user out of the website.
-![image](https://user-images.githubusercontent.com/56043146/110336421-dab28580-804a-11eb-87bd-acb7c4673561.png)
-![image](https://user-images.githubusercontent.com/56043146/110336445-e1d99380-804a-11eb-9782-1a4b6cf03b0b.png)
-![image](https://user-images.githubusercontent.com/56043146/110336474-e7cf7480-804a-11eb-90e1-54959869ba7f.png)
-![image](https://user-images.githubusercontent.com/56043146/110336502-edc55580-804a-11eb-880d-8f434009b2f8.png)
-
-![image](https://user-images.githubusercontent.com/56043146/110336516-f28a0980-804a-11eb-9112-933b8c68242f.png)
-
-![image](https://user-images.githubusercontent.com/56043146/110420003-66b2c480-80c0-11eb-8ee5-2b9e1713d00a.png)
-
-![image](https://user-images.githubusercontent.com/56043146/110420025-70d4c300-80c0-11eb-8337-fd300e60f23e.png)
-![image](https://user-images.githubusercontent.com/56043146/110420049-821dcf80-80c0-11eb-9bdc-0c26fd30c35f.png)
 
 
+![image](https://user-images.githubusercontent.com/56025143/110335467-e3568c00-8049-11eb-96ff-63f81b7c1d15.png)
+Landing Page
 
+![image](https://user-images.githubusercontent.com/56025143/110335636-1567ee00-804a-11eb-9dba-251917eab9b2.png)
+Login Page
 
+![image](https://user-images.githubusercontent.com/56025143/110335768-38929d80-804a-11eb-9dcb-9dd6c3899472.png)
+Register page
 
+![image](https://user-images.githubusercontent.com/56025143/110335896-58c25c80-804a-11eb-9b6a-334db438bb29.png)
+Home Page
+
+![image](https://user-images.githubusercontent.com/56025143/110336026-798ab200-804a-11eb-8e91-275cb88ea76c.png)
+Dashboard 1
+
+![image](https://user-images.githubusercontent.com/56025143/110336150-9626ea00-804a-11eb-8f39-b5545876c4b0.png)
+Dashboard 2
+
+![image](https://user-images.githubusercontent.com/56025143/110336238-adfe6e00-804a-11eb-8021-7a98c8c479fa.png)
+Dashboard 3
+
+![image](https://user-images.githubusercontent.com/56025143/110336310-be164d80-804a-11eb-8beb-ce78ea586278.png)
+Settings Page
 
---
+++ index.js
@@ -0,0 +1,269 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const JSAlert = require('js-alert');
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+global.username="";
+global.budget_val=0;
+global.flag=0;
+mongoose.connect('mongodb+srv://g3:g3@cluster0.9owf4.mongodb.net/userDB',{ useNewUrlParser: true, useUnifiedTopology: true } ); 
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String,
+  budget: Number
+});
+
+const expenseSchema = new mongoose.Schema ({
+  email: String,
+  category: String,
+  cost: Number,
+  item: String,
+  date:
+  {
+    type:Date, 
+    default:Date.now()
+  },
+  delete:Boolean
+});
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+
+const User = new mongoose.model("User", userSchema);
+const Expense = new mongoose.model("Expense",expenseSchema);
+app.get("/", function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get("/login", function(req, res){
+  res.sendFile(__dirname+'/login.html');
+});
+app.get("/register", function(req, res){
+  res.sendFile(__dirname+'/register.html');
+});
+app.get("/home", function(req, res){
+  if(flag==1){res.sendFile(__dirname+'/home.html');}
+  else{res.redirect('/')}
+});
+app.get("/settings", function(req, res){
+  if(flag==1){res.sendFile(__dirname+'/settings.html');}
+  else{res.redirect('/')}
+});
+app.get("/dashboard", function(req, res){
+  if(flag==1)
+  {   
+    Expense.find({ email: username }, function(err, results) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(results);
+          res.render('dashboard',{budget:budget_val,results:results});
+        }
+      });
+  }
+  else{res.redirect('/')}
+});
+app.get("/signout", function(req, res){
+  if(flag==1)
+  {
+    flag=0;
+    console.log(flag);
+    res.redirect('/');
+    
+  }
+  else{res.redirect('/')}
+});
+app.post("/register", function(req, res){
+  username = req.body.username;
+  const newUser =  new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      flag=1;
+      budget_val=0;
+      res.sendFile(__dirname+'/home.html');
+    }
+  });
+
+});
+app.post("/login", function(req, res){
+  username = req.body.username;
+  const password = req.body.password;
+  console.log(username);
+  console.log(password);
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          budget_val = foundUser.budget
+          flag=1;
+          res.sendFile(__dirname+'/home.html');
+        }
+       
+      }
+    }
+  });
+});
+app.post("/budget",function(req,res){
+  var budget= Number(req.body.budget);
+  budget_val = budget;
+  console.log(budget)
+  User.updateOne({email:username},{budget:budget},function(err){
+    if(err){
+         console.log(err);
+       }
+    else{
+         console.log('All good!')
+       }
+     })
+  res.sendFile(__dirname+'/settings.html');
+})
+app.post("/expenditure",function(req,res){
+  var category = req.body.cat;
+  var item = req.body.item;
+  var cost = Number(req.body.cost);
+  console.log(category);
+  console.log(item);
+  console.log(cost);
+  const newExpense =  new Expense({
+    email: username,
+    category: category,
+    item: item,
+    cost: cost,
+    delete:0
+  });
+  newExpense.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      budget_val = budget_val-cost;
+      User.updateOne({email:username},{budget:budget_val},function(err){
+        if(err){
+             console.log(err);
+           }
+        else{
+             console.log('All good!')
+           }
+         })
+      res.sendFile(__dirname+'/settings.html');
+    }
+  });
+  
+})
+app.post("/checkbox",function(req,res){
+  var id = req.body.checkbox;
+  Expense.findOne({_id: id}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.delete === false) {
+          budget_val = Number(budget_val)+Number(foundUser.cost);
+          Expense.updateOne({_id:id},{delete:1},function(err){
+            if(err){
+                 console.log(err);
+               }
+            else{
+                 console.log('All good!')
+               }
+             });
+             User.updateOne({email:username},{budget:budget_val},function(err){
+              if(err){
+                   console.log(err);
+                 }
+              else{
+                   console.log('All good!')
+                 }
+               });
+               res.redirect('/dashboard');
+        }
+        else{
+          budget_val = Number(budget_val)-Number(foundUser.cost);
+          Expense.updateOne({_id:id},{delete:false},function(err){
+            if(err){
+                 console.log(err);
+               }
+            else{
+                 console.log('All good!')
+               }
+             });
+          
+          User.updateOne({email:username},{budget:budget_val},function(err){
+            if(err){
+                 console.log(err);
+               }
+            else{
+                 console.log('All good!')
+               }
+             });
+             res.redirect('/dashboard');
+        }
+      }
+    }
+  });
+})
+app.post("/updatecontents",function(req,res)
+{
+  console.log(req.body.inametobe);
+  console.log(req.body.updatename);
+  console.log(req.body.updatecost);
+  const toupdate=req.body.inametobe;
+  const newname=req.body.updatename;
+  const newcost=req.body.updatecost;
+  
+  Expense.findOne({email: username,item:toupdate},function(err, foundUser){
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      var bud=foundUser.cost;
+      budget_val = Number(budget_val)+Number(newcost)-Number(bud);
+      console.log('All good!')
+    }
+  });
+  User.updateOne({email:username},{budget:budget_val},function(err){
+    if(err){
+         console.log(err);
+       }
+    else{
+         console.log('All good!')
+       }
+     });
+  Expense.updateOne({email: username,item:toupdate},{item:newname,cost:newcost}, function(err, foundUser){
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      console.log('All good!')
+    }
+  });
+  res.redirect('/dashboard');
+
+      });
+      
+
+    
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
